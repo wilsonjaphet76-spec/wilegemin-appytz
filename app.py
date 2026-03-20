fe53dd9c58965e3344f768c0ca704a0c1b6e6479
wilsonjaphet76-spec <wilsonjaphet76@gmail.com>
2026-03-20 07:41:21 +0300

    Correct model assignment in app.pymodel = genai.GenerativeModel('gemini-1.5-flash')

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ st.write("Karibu Wilson! Huu ni mfumo wako wa AI unakuwezesha kuuliza chochote."
 api_key = st.secrets["GOOGLE_API_KEY"]
 genai.configure(api_key=api_key)
 
-model =
+model =model = genai.GenerativeModel('gemini-1.5-flash')
+
 model = genai.GenerativeModel('gemini-1.5-flash')
 
 if "messages" not in st.session_state:
